d399d9e314b94ef16c976e284427b5cd9bf4c924
jooeun kim <jay311311@gmail.com>
2022-03-02 14:11:51 +0900

    firebase의 데이터 읽어오기

--- Firebase_101/ViewController.swift
+++ Firebase_101/ViewController.swift
@@ -10,12 +10,25 @@ import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dbLabel: UILabel!
     var db =  Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        db.child("firstData").observeSingleEvent(of: .value, with: { snapshot in print("-->\(snapshot)")})
+        updateDBLabel()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateDBLabel() {
+        db.child("firstData").observeSingleEvent(of: .value) { snapshot in print("-->\(snapshot)")
+            let value =  snapshot.value as? String ??  "찾을수 없음"
+            DispatchQueue.main.async {
+                self.dbLabel.text = value            }
+            
+        }
+        
+       
+     
     }
 
 
