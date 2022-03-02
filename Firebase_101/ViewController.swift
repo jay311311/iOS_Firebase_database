ae0fbfbb77e76222cbfca70f8b8aadc93c9fd559
jooeun kim <jay311311@gmail.com>
2022-03-02 15:45:00 +0900

    데이터 구조화하기(Dictionary) + firebase에 데이터 write

--- Firebase_101/ViewController.swift
+++ Firebase_101/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateDBLabel()
+        setDBData()
+        saveCustomer()
         // Do any additional setup after loading the view.
     }
     
@@ -23,14 +25,62 @@ class ViewController: UIViewController {
         db.child("firstData").observeSingleEvent(of: .value) { snapshot in print("-->\(snapshot)")
             let value =  snapshot.value as? String ??  "찾을수 없음"
             DispatchQueue.main.async {
-                self.dbLabel.text = value            }
-            
+                self.dbLabel.text = value
+            }
         }
-        
-       
-     
     }
-
-
 }
 
+
+extension ViewController{
+    func setDBData(){
+        let user = db.child("user")
+        user.child("userName").setValue(["firstName" : "Jooeun","lastName" : "kim" ])
+        user.child("age").setValue(26)
+    }
+    
+    func saveCustomer(){
+        
+        let books = [Book(title: "harry potter", author: "J.K.Rolling"),
+                     Book(title: "Art Of Parents", author: "Dennis")]
+        let customer1 =  Customer(id: Customer.id, name: "Som", books: books)
+        Customer.id += 1
+        let customer2 =  Customer(id: Customer.id, name: "Kim", books: books)
+        Customer.id += 1
+        let customer3 =  Customer(id: Customer.id, name: "Mike", books: books)
+        Customer.id += 1
+
+        db.child("customer").child("\(customer1.id)").setValue(customer1.toDictionary)
+        db.child("customer").child("\(customer2.id)").setValue(customer2.toDictionary)
+        db.child("customer").child("\(customer3.id)").setValue(customer3.toDictionary)
+
+    
+        
+    }
+}
+
+struct Customer{
+    let id : Int
+    let name : String
+    let books : [Book]
+    
+    var toDictionary :[String:Any]{
+        let bookArray  =  books.map{ book in book.toDictionary}
+        let dict:[String:Any] = ["id" : id, "name" : name, "books": bookArray]
+        return dict
+    }
+    static var id = 0
+    
+}
+
+struct Book{
+    let title: String
+    let author : String
+    
+    var toDictionary:[String : Any] {
+        let dict:[String : Any]  = ["title":title, "author" : author]
+        return dict
+    }
+    
+    
+}
