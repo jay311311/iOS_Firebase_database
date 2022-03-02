d7c9d3be705d1fed619f4ec1077cca8c8c545497
jooeun kim <jay311311@gmail.com>
2022-03-02 13:15:43 +0900

    firebase connect with Xcode

--- Firebase_101/ViewController.swift
+++ Firebase_101/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var db =  Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        db.child("firstData").observeSingleEvent(of: .value, with: { snapshot in print("-->\(snapshot)")})
         // Do any additional setup after loading the view.
     }
 
